9103ac4ae3e9c3c07d8ec0904f3e45789487fc1d
Sungsik Nam <jmyl@me.com>
2026-07-01 15:09:06 +0200

    Avoid injected Markdown formatter timeouts
    Use Prettier only for Markdown autoformatting so Obsidian notes do not hit Conform's injected formatter timeout on save.

---
+++ dot_config/nvim/lua/kickstart/plugins/format.lua
@@ -0,0 +1,67 @@
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true, cmake = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        bash = { 'shfmt' },
+        yaml = { 'prettier' },
+        tex = { 'latexindent' },
+        nix = { 'alejandra' },
+        proto = { 'buf' },
+        cmake = { 'gersemi' },
+        -- Keep Markdown formatting simple for Obsidian notes. The injected
+        -- formatter can time out while scanning large wiki-style notes, and
+        -- Prettier already handles the Markdown document itself.
+        markdown = { 'prettier' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { '--editorconfig' },
+        },
+        latexindent = {
+          command = 'latexindent',
+          args = { '-' },
+          stdin = true,
+        },
+        ['clang-format'] = {
+          args = { '--style=file:' .. vim.fn.expand('~/.clang-format') },
+        },
+      },
+    },
+  },
+}
